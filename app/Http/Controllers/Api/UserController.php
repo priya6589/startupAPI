0a64a0b15701127c3d9b516d905d728b2f52c1ec
Binarydata12 <binarydata.sumeet2@gmail.com>
2023-04-05 17:09:53 +0530

    latest commit

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -21,10 +21,6 @@ use Carbon\Carbon;
 
 class UserController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:api');
-    }
     public function userRegister(Request $request)
     {
         try {
