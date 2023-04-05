7d6b2ab5df74ecdfee3c53ca03ff61dd93977503
Binarydata12 <binarydata.sumeet2@gmail.com>
2023-04-05 18:19:14 +0530

    latest commit

--- routes/api.php
+++ routes/api.php
@@ -23,10 +23,15 @@ Route::group(['middleware' => 'api'], function () {
     Route::post('startup-register', [App\Http\Controllers\Api\UserController::class, 'startup_register']);
     Route::post('register', [App\Http\Controllers\Api\UserController::class, 'userRegister']);
     Route::post('investor-register', [App\Http\Controllers\Api\UserController::class, 'investor_register']);
-    Route::get('user-login', [App\Http\Controllers\Api\UserController::class, 'user_login']);
-    Route::get('user-login', [App\Http\Controllers\Api\UserController::class, 'user_login']);
     Route::get('send-otp', [App\Http\Controllers\Api\UserController::class, 'send_otp']);
     Route::post('confirm-otp', [App\Http\Controllers\Api\UserController::class, 'confirm_otp']);
+    Route::post('save-contact', [App\Http\Controllers\Api\UserController::class, 'save_contact']);
+
+});
+
+Route::group(['middleware' => ['jwt.verify']], function () {
+
+     Route::get('user-login', [App\Http\Controllers\Api\UserController::class, 'user_login']);
 
     Route::post('update-profile/{id}', [App\Http\Controllers\Api\UserController::class, 'update_profile']);
     Route::get('single-user/{id}', [App\Http\Controllers\Api\UserController::class, 'get_single_user']);
@@ -38,8 +43,6 @@ Route::group(['middleware' => 'api'], function () {
     Route::post('document-upload/{id}', [App\Http\Controllers\Api\UserController::class, 'document_upload']);
 
     Route::post('update-business-details/{id}', [App\Http\Controllers\Api\UserController::class, 'business_detail_update']);
-
-    Route::post('save-contact', [App\Http\Controllers\Api\UserController::class, 'save_contact']);
 
      // Countries route 
      Route::get('countries',[App\Http\Controllers\Api\CountryController::class,'all_countries']);
