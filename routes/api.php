2a6d61a8eed3fb2ff8be697dea5129976ecc1013
Binarydata12 <binarydata.sumeet2@gmail.com>
2023-04-06 16:47:29 +0530

    latest commit

--- routes/api.php
+++ routes/api.php
@@ -31,8 +31,6 @@ Route::group(['middleware' => 'api'], function () {
 
 Route::group(['middleware' => ['jwt.verify']], function () {
 
-     Route::post('user-login', [App\Http\Controllers\Api\UserController::class, 'user_login']);
-
     Route::post('update-profile/{id}', [App\Http\Controllers\Api\UserController::class, 'update_profile']);
     Route::get('single-user/{id}', [App\Http\Controllers\Api\UserController::class, 'get_single_user']);
     Route::post('join_to_invest', [App\Http\Controllers\Api\UserController::class, 'join_to_invest']);
